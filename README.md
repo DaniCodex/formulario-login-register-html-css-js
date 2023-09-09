5202fc11445895fb8ae3c5c7d71f919068e72d90
DaniCodex <revolutiondaniel19@gmail.com>
2023-09-08 22:20:36 -0500

    SUBIENDO PROYECTO FINALIZADO

---
+++ README.md
@@ -0,0 +1,1 @@
+# formulario-login-register-html-css-js
---
+++ script.js
@@ -0,0 +1,15 @@
+const btnSignIn = document.getElementById("sign-in"),
+      btnSignUp = document.getElementById("sign-up"),
+      formRegister = document.querySelector(".register"),
+      formLogin = document.querySelector(".login");
+
+btnSignIn.addEventListener("click", e => {
+    formRegister.classList.add("hide");
+    formLogin.classList.remove("hide")
+})
+
+
+btnSignUp.addEventListener("click", e => {
+    formLogin.classList.add("hide");
+    formRegister.classList.remove("hide")
+})
